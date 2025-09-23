2a7a07f74261cf8324d6bc5b3eafe015f56be6dc
Louis <Lou.ikwuegbu@gmail.com>
2025-09-23 23:04:52 +0100

    restructured and fixed widget hierarchy

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,148 @@
-import 'package:flutter/material.dart';
 
-void main()=>runApp(Quizzler());
+import 'package:flutter/material.dart';
+//TODO: Step 2 - Import the rFlutter_Alert package here.
+//import 'quiz_brain.dart';
+
+//QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.grey.shade900,
-          body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-            Expanded(
-              flex: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Center(
-                  child: Text(style: TextStyle(
-                    color: Colors.white),
-                   'This is where the question text will go'),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  // void checkAnswer(bool userPickedAnswer) {
+  //   bool correctAnswer = quizBrain.getCorrectAnswer();
+
+    //setState(() {
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
+      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+      //HINT! Step 4 Part B is in the quiz_brain.dart
+      //TODO: Step 4 Part C - reset the questionNumber,
+      //TODO: Step 4 Part D - empty out the scoreKeeper.
+
+      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
+  //     if (userPickedAnswer == correctAnswer) {
+  //       scoreKeeper.add(Icon(
+  //         Icons.check,
+  //         color: Colors.green,
+  //       ));
+  //     } else {
+  //       scoreKeeper.add(Icon(
+  //         Icons.close,
+  //         color: Colors.red,
+  //       ));
+  //     }
+  //     quizBrain.nextQuestion();
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                //quizBrain.getQuestionText(),
+                "this is where the question goes",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
                 ),
               ),
             ),
-            
-                Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Container(
-                      color: Colors.green,
-                      child: TextButton(
-                        onPressed: () {
-                          print('pressed');
-                      }, child: const Text(style:TextStyle(color: Colors.white), "True")
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 30
-              ),
-               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                 children: [
-                    Expanded(
-                      child: Container(
-                        color: Colors.red,
-                        child: TextButton(
-                          onPressed: () {
-                            print('pressed');
-                          }, child: const Text(style:TextStyle(color: Colors.white), "False")
-                        ),
-                      ),
-                    ),
-                 ],
-               ),
-            ],
           ),
         ),
-      )
-    ); 
+        Expanded(
+          child: Container(
+            color: Colors.green,
+            //child: Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: TextButton(
+                  //textColor: Colors.white,
+                  child: Text(
+                    'True',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  onPressed: () {
+                    //The user picked true.
+                    //checkAnswer(true);
+                  },
+                ),
+              ),
+            //),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        
+        Expanded(
+          child: Container(
+            color: Colors.red,
+            //child: Expanded(
+                // child: Padding(
+                //   padding: EdgeInsets.all(15.0),
+                  child: TextButton(
+                    child: Text(
+                      'False',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      //The user picked false.
+                      //checkAnswer(false);
+                    },
+                  ),
+                ),
+              //), expanded close
+          ),
+        
+        
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
   }
 }
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
